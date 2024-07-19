9d36af27d7dbdf8429658ddaca66fcc735641672
ChihChengLiang <chihchengliang@gmail.com>
2024-07-19 14:38:23 -0700

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 mod client_server;
+mod types;
 
 pub use client_server::{
     rocket, setup, Cipher, CipherSubmission, DecryptionShare, DecryptionShareSubmission,
     DecryptionSharesMap, RegisteredUser, RegistrationOut, ServerKeyShare, ServerResponse, User,
     TOTAL_USERS,
 };
+
+pub use types::{Cipher, ClientKey, DecryptionShare, FheUint8, ServerKeyShare, UserId};
+
+#[cfg(test)]
+mod tests;
